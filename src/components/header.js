8254ecf9e16e9d94abed984331594891d0964a19
baekjinyeong <jinyeong1612@cttd.co.kr>
2019-05-23 10:32:09 +0900

    작업중

--- src/components/header.js
+++ src/components/header.js
@@ -5,9 +5,10 @@ class Header extends Component {
     return(
       <header id="header" className="header">
         <h1 className="App-logo">
-          <svg width="82" height="82" version="1.1" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="41" cy="41" r="40" stroke="black" fill="transparent" stroke-width="1" />
+          <svg width="82" height="82" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 82 82">
+            <rect x="0" y="0" width="60" height="60" fill="transparent" stroke="black" strokeWidth="3"/>
           </svg>
+          <span class="logo-text">J</span>
         </h1>
       </header>
     );
