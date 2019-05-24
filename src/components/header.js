a6bb0efde4fefe35cf9c25bfdaa61edad9f549d0
baekjinyeong <jinyeong1612@cttd.co.kr>
2019-05-24 11:03:34 +0900

    작업중

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-
 class Header extends Component {
   render() {
     return(
       <header id="header" className="header">
         <h1 className="App-logo">
           <svg width="82" height="82" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 82 82">
-            <rect x="0" y="0" width="60" height="60" fill="transparent" stroke="black" strokeWidth="3"/>
+            <rect x="0" y="0" width="60" height="60" fill="transparent" stroke="#e03131" strokeWidth="3"/>
           </svg>
-          <span class="logo-text">J</span>
+          <span class="logo-text">JINYEONG</span>
         </h1>
       </header>
     );
